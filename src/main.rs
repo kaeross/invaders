3e259e7ee04bad4e098d6cc219496be15b03988f
kaeross <kaeross@icloud.com>
2022-01-06 10:33:22 +0000

    set up terminal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use crossterm::cursor::{Hide, Show};
+use crossterm::{
+    terminal,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
 use rusty_audio::Audio;
 use std::error::Error;
+use std::io;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -12,7 +19,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     audio.play("startup");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
     // Cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }

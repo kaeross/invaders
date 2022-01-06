252360bbc606e21325f3f0083ff87cd321820554
kaeross <kaeross@icloud.com>
2022-01-06 12:01:33 +0000

    Add initial gameloop

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use crossterm::cursor::{Hide, Show};
 use crossterm::{
+    cursor::{Hide, Show},
+    event,
+    event::{Event, KeyCode},
     terminal,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
 use rusty_audio::Audio;
-use std::error::Error;
 use std::io;
+use std::{error::Error, time::Duration};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -20,15 +22,45 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.play("startup");
 
     // Terminal
+    let stdout = init_terminal()?;
+
+    // Game loop
+    'gameloop: loop {
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    // Cleanup
+    cleanup(audio, stdout)
+}
+
+fn init_terminal() -> Result<std::io::Stdout, Box<dyn Error>> {
     let mut stdout = io::stdout();
+
     terminal::enable_raw_mode()?;
+
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
-    // Cleanup
+    Ok(stdout)
+}
+
+fn cleanup(audio: rusty_audio::Audio, mut stdout: std::io::Stdout) -> Result<(), Box<dyn Error>> {
     audio.wait();
+
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
+
     terminal::disable_raw_mode()?;
+
     Ok(())
 }
